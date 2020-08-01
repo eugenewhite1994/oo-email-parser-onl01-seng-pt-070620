5fcb98d58ebde1c1eb663f6de952b9c528176fff
Eugene White <eugenewhite1994@gmail.com>
2020-08-01 21:15:35 +0000

    Automatically backed up by Learn

---
+++ lib/email_parser.rb
@@ -0,0 +1,11 @@
+class EmailAddressParser
+  attr_accessor :email_addresses
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse
+    @email_addresses = email_addresses.split(/, | /).uniq
+  end
+end
